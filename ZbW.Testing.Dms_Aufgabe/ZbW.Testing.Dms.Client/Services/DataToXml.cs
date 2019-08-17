ef8b65ad2217d2f9fa66b90aa99a87aae7152c64
Thomas Canal <t.canal@bst.ch>
2019-08-17 12:19:21 +0200

    Daten in XML schreiben

---
+++ ZbW.Testing.Dms_Aufgabe/ZbW.Testing.Dms.Client/Services/DataToXml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZbW.Testing.Dms.Client.Services
+{
+    using System.IO;
+    using System.Xml;
+    using System.Xml.Serialization;
+
+    using ZbW.Testing.Dms.Client.Model;
+
+    class DataToXml
+    {
+        public string MetadatenSchreiben(MetadataItem metadataItem)
+        {
+            var xmlserializer = new XmlSerializer(typeof(MetadataItem)); //Objekt MetadataIteam schreiben
+            //var stringWriter = new StringWriter();
+            //var datei = XmlWriter.Create(stringWriter);
+
+            var filePfad = metadataItem.FilePfad;
+            FileStream datei = File.Create(filePfad);
+            //var xmlSchreiben = stringWriter.ToString();
+
+            xmlserializer.Serialize(datei, metadataItem);
+            datei.Close();
+
+            return filePfad;
+        }
+
+        //public Metadaten LesenMetadten(string pfad)
+        //{
+
+        //}
+
+        //public void XmlSpeichern()
+        //{
+
+        //}
+        //public void AlsXmlSpeichern (string seriali)
+
+
+
+        /****************Beispiel aus MSDN!*************************/
+        //public static void WriteXML()
+        //{
+        //    Book metadataItem = new Book();
+        //    metadataItem.title = "Serialization Overview";
+        //    XmlSerializer xmlserializer = new XmlSerializer(typeof(Book));
+
+        //    var path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "//SerializationOverview.xml";
+        //    FileStream datei = File.Create(path);
+
+        //    xmlserializer.Serialize(datei, metadataItem);
+        //    datei.Close();
+        //}
+    }
+}
