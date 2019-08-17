124c099350b0945f36975e15a1f7021bb952102e
Thomas Canal <t.canal@bst.ch>
2019-08-17 09:53:29 +0200

    Properties MetadataItem

--- ZbW.Testing.Dms_Aufgabe/ZbW.Testing.Dms.Client/Model/MetadataItem.cs
+++ ZbW.Testing.Dms_Aufgabe/ZbW.Testing.Dms.Client/Model/MetadataItem.cs
@@ -1,7 +1,55 @@
 ﻿namespace ZbW.Testing.Dms.Client.Model
 {
+    using System;
+
     internal class MetadataItem
     {
-        // TODO: Write your Metadata properties here
+        public string FilePfad { get; set; }
+
+        public string Bezeichnung { get; set; }
+
+        public DateTime ValutaDatum { get; set; }
+
+        public string Typ { get; set; }
+
+        public string Stichwoerter { get; set; }
+
+        public DateTime Erfassungsdatum { get; set; }
+
+        public string Benutzer { get; set; }
+
+        public bool DateiAnschliessendLöschen { get; set; }
+
+        public MetadataItem(string filePfad, string bezeichnung, DateTime valutaDatum, string typ, 
+                            DateTime erfassungsdatum, string benutzer, bool dateiAnschliessendLöschen)
+        {
+            FilePfad = filePfad;
+            Bezeichnung = bezeichnung;
+            ValutaDatum = valutaDatum;
+            Typ = typ;
+            Erfassungsdatum = erfassungsdatum;
+            Benutzer = benutzer;
+            DateiAnschliessendLöschen = dateiAnschliessendLöschen;
+        }
+
+        public MetadataItem(
+            string filePfad,
+            string bezeichnung,
+            DateTime valutaDatum,
+            string typ,
+            string stichwoerter,
+            DateTime erfassungsdatum,
+            string benutzer,
+            bool dateiAnschliessendLöschen)
+        {
+            FilePfad = filePfad;
+            Bezeichnung = bezeichnung;
+            ValutaDatum = valutaDatum;
+            Typ = typ;
+            Stichwoerter = stichwoerter;
+            Erfassungsdatum = erfassungsdatum;
+            Benutzer = benutzer;
+            DateiAnschliessendLöschen = dateiAnschliessendLöschen;
+        }
     }
 }
